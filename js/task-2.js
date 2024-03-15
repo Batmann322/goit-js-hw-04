131c800f7c41289a427670e33bb13f5a3d9a84fc
Nazar <n.kopilchak@gmail.com>
2024-03-15 19:09:42 +0200

    start

---
+++ js/task-2.js
@@ -0,0 +1,10 @@
+const apartment = {
+  descr: "Spacious apartment in the city center",
+  rating: 4,
+  price: 2153,
+};
+
+const keys = apartment;
+console.log(keys.Object.keys(apartment));
+const values = apartment;
+console.log(apartment.Object.values(apartment));
