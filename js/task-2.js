cd1532cf48d46245c41fae2f43d36013a97f7a33
Nazar <n.kopilchak@gmail.com>
2024-03-15 20:19:40 +0200

    done

--- js/task-2.js
+++ js/task-2.js
@@ -1,10 +1,35 @@
-const apartment = {
-  descr: "Spacious apartment in the city center",
-  rating: 4,
-  price: 2153,
-};
+function calcAverageCalories(days) {
+  let totalCalories = 0;
+  let daysCalories = days.length;
 
-const keys = apartment;
-console.log(keys.Object.keys(apartment));
-const values = apartment;
-console.log(apartment.Object.values(apartment));
+  for (let day of days) {
+    totalCalories += day.calories;
+  }
+  return totalCalories / daysCalories;
+}
+
+console.log(
+  calcAverageCalories([
+    { day: "monday", calories: 3010 },
+    { day: "tuesday", calories: 3200 },
+    { day: "wednesday", calories: 3120 },
+    { day: "thursday", calories: 2900 },
+    { day: "friday", calories: 3450 },
+    { day: "saturday", calories: 3280 },
+    { day: "sunday", calories: 3300 },
+  ])
+); // 3180
+
+console.log(
+  calcAverageCalories([
+    { day: "monday", calories: 2040 },
+    { day: "tuesday", calories: 2270 },
+    { day: "wednesday", calories: 2420 },
+    { day: "thursday", calories: 1900 },
+    { day: "friday", calories: 2370 },
+    { day: "saturday", calories: 2280 },
+    { day: "sunday", calories: 2610 },
+  ])
+); // 2270
+
+console.log(calcAverageCalories([])); // 0
